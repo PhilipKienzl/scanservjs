48115e13d2966c68dcf5b9235a47d3649963d1b1
Philip Kienzl <56354488+PhilipKienzl@users.noreply.github.com>
2019-11-06 21:56:49 +0100

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "${1}" == "odd" ] 
 then
-mv out*.tif odd/
+mv data/out*.tif odd/
 echo "Moved to odd"
 elif [ "${1}" == "even" ]
 then
-mv out*.tif even/
+mv data/out*.tif even/
 echo "Moved to even"
 fi
