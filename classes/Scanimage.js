4f49aae8fd1333aeca1aeb6aaec40ffe24eeeaf0
Philip Kienzl <56354488+PhilipKienzl@users.noreply.github.com>
2019-11-07 08:36:44 +0100

    Update Scanimage.js

--- classes/Scanimage.js
+++ classes/Scanimage.js
@@ -47,13 +47,13 @@ module.exports = function () {
 		
 		switch(scanRequest.adf) {
   			case '2':
-    			cmd += ' --batch=data/odd/out%d --source "Automatic Document Feeder"';
+    			cmd += ' --batch=' + Config.OutputDirectory + 'odd/out%d --source "Automatic Document Feeder"';
     			break;
 			case '3':
-  			cmd += ' --batch=data/even/out%d --source "Automatic Document Feeder"';
+  			cmd += ' --batch=' + Config.OutputDirectory + 'even/out%d --source "Automatic Document Feeder"';
     			break;
 			case '4':
-			cmd = './merge.sh';
+			cmd = './merge.sh ' + Config.OutputDirectory;
 			break;
 			default:
 			// Last
